c296d484f0f879c5b7d4c754d547c344c8116cda
beatricezz <beatrice.zorzoli@mail.polimi.it>
2022-10-04 10:04:00 +0200

    continuo

--- fetch_pubmed.py
+++ fetch_pubmed.py
@@ -33,12 +33,15 @@ else:
 # # Fetch articles
 
 # In[4]:
-
-
-query = "random"
-max_results = 10
 pubmed = PubMed()
-results = pubmed.query(query, max_results=max_results)
+print("----------")
+sel=input("Insert the term that you want to find: ")
+num=input("Insert how many articles do you want to find: ")
+results = pubmed.query(sel, max_results=int(num))
+print("----------")
+
+#pubmed = PubMed()
+#results = pubmed.query(query, max_results=max_results)
 
 
 # # Populate the database
@@ -59,9 +62,16 @@ for result in results:
     
     # print("got article with title", title, "pid", pid, "doi", doi, "abstract", abstract, "pub_date", pub_date, "authors", authors)
     article = e_health.article.Article(title = title, pubmed_id=pid, 
-                                             doi = doi, abstract = abstract, 
+                                             doi = doi, abstract = abstract,
                                              pub_date = pub_date, authors = authors )
-    print("made article", article)
+
+    print('\n----------------\n')
+    print("made article:", "\ntitle:", article.title,
+                          "\npubmed id:", article.pubmed_id,
+                          "\ndoi:", article.doi,
+                          "\ndate:", article.pub_date,
+                          "\nauthors:", article.authors
+                          )
     articles.append(article)
 
 db.insert_documents_and_commit(articles)
@@ -69,17 +79,7 @@ db.insert_documents_and_commit(articles)
 
 # In[6]:
 
-
 print("db contains", len(db.get_articles()), "articles")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
